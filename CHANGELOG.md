3f110d2f5c71e57bdfd62a19850b3ce7cfa50397
Jannik Zschiesche <j.zschiesche@21torr.com>
2026-01-22 17:52:42 +0100

    Disable Janus autorun
    It fails to often. Replace it with a big message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+2.0.1
+=====
+
+* (improvement) Disable automatically running janus, as it often fails.
+
+
 2.0.0
 =====
 
--- src/Composer/JanusPlugin.php
+++ src/Composer/JanusPlugin.php
@@ -3,7 +3,6 @@
 namespace Janus\Composer;
 
 use Composer\Composer;
-use Composer\DependencyResolver\Operation\InstallOperation;
 use Composer\DependencyResolver\Operation\OperationInterface;
 use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
@@ -13,9 +12,6 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
-use Janus\Command\InitializeCommand;
-use Janus\Package\PackageType;
-use Symfony\Component\Process\Process;
 
 /**
  * @final
@@ -68,11 +64,6 @@ class JanusPlugin implements PluginInterface, EventSubscriberInterface
 	 */
 	private function isJanusUpdate (OperationInterface $operation) : bool
 	{
-		if ($operation instanceof InstallOperation)
-		{
-			return "21torr/janus" === $operation->getPackage()->getName();
-		}
-
 		if ($operation instanceof UpdateOperation)
 		{
 			return "21torr/janus" === $operation->getTargetPackage()->getName();
@@ -94,91 +85,12 @@ class JanusPlugin implements PluginInterface, EventSubscriberInterface
 		$this->hadJanusOperation = false;
 
 		$io = $event->getIO();
-		$io->write("\n<fg=magenta>Janus update detected, running janus update</>\n");
-
-		// please note, that the detection can fail: composer defaults to "library", if it's not set
-		$packageType = PackageType::tryFromComposerType(
-			$event->getComposer()->getPackage()->getType(),
-		);
-
-		if (null === $packageType)
-		{
-			$selected = $io->select(
-				"What are you currently using?",
-				InitializeCommand::ALLOWED_TYPES,
-				"library",
-			);
-			$packageType = PackageType::tryFromComposerType(InitializeCommand::ALLOWED_TYPES[$selected] ?? null);
-		}
-		else
-		{
-			$io->write(\sprintf(
-				"Detected package type <fg=yellow>%s</>",
-				$packageType->value,
-			));
-		}
-
-		$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-		\assert(\is_string($vendorDir));
-
-		$success = $this->runJanus($io, $vendorDir, $packageType);
-
-		if ($success)
-		{
-			$io->write("\n<fg=green>Janus installation complete.</>\n");
-		}
-		else
-		{
-			$io->writeError("\n<fg=red>Janus installation failed, please run it manually: `composer exec janus init`</>\n");
-		}
-	}
-
-	/**
-	 * Runs Janus
-	 */
-	private function runJanus (
-		IOInterface $io,
-		string $vendorDir,
-		?PackageType $type,
-	) : bool
-	{
-		$command = [
-			$this->findJanusExecutable($vendorDir),
-			"init",
-		];
-
-		if (null !== $type)
-		{
-			$command[] = $type->value;
-		}
-
-		$command[] = "--no-auto-install";
-		$command[] = "--ansi";
-
-		$output = new Process($command);
-		$output->run(
-			static function ($type, $buffer) use ($io) : void
-			{
-				$io->write($buffer);
-			},
-		);
-
-		return $output->isSuccessful();
-	}
-
-	/**
-	 * Finds the path to the janus executable
-	 */
-	private function findJanusExecutable (string $vendorDir) : string
-	{
-		// first check if it's installed in the project via composer
-		if (is_dir("{$vendorDir}/bin/janus"))
-		{
-			return "{$vendorDir}/bin/janus";
-		}
-
-		// otherwise just fetch the executable from the library and run it
-		return \dirname(__DIR__, 2) . "/bin/janus";
+		$io->write("\n<fg=magenta>Janus update detected, you need to run janus:</>\n");
+		$io->write("\n");
+		$io->write("  ╭───────────────────────────────────────────╮\n");
+		$io->write("  │ <fg=magenta>$ composer exec janus init</> │\n");
+		$io->write("  ╰───────────────────────────────────────────╯\n");
+		$io->write("\n");
 	}
 
 	/**
